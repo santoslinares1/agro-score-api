86b303bc89aee1b3698724ce5ff5ab9bf1534860
santoslinares1 <slinares@mi.unc.edu.ar>
2026-08-04 00:50:19 -0300

    fix: register app health controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ContactModule } from './contact/contact.module';
 import { AccessRequestModule } from './access-request/access-request.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -67,5 +69,7 @@ import { AccessRequestModule } from './access-request/access-request.module';
     ContactModule,
     AccessRequestModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
